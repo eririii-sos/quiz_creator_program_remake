8ac9ddca498399570802920425120bc458de2da9
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-24 23:56:45 +0800

    Imported the libraries to be used

--- source_code/quiz_creator_program_remake.py
+++ source_code/quiz_creator_program_remake.py
@@ -3,3 +3,7 @@
 # Ask another question until the user chose to exit.
 
 # (Optional) Install library/libraries that you'll use in the program.
+
+# Import dependencies
+from colorama import init, Fore, Style, Back
+import os
