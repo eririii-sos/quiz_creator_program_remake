2b9d1dd48731e49f5fb44b6f6c00a1cef6bda154
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-24 23:49:25 +0800

    Added initial guidelines

---
+++ source_code/quiz_creator_program_remake.py
@@ -0,0 +1,5 @@
+# Create a program that asks user for a question, then ask for 4 possible answer and the correct answer.
+# Write the collected data to a text file.
+# Ask another question until the user chose to exit.
+
+# (Optional) Install library/libraries that you'll use in the program.
